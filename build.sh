c2a450c3d28f639871c87177ecbaa8bc775aee48
Vladimir Ovechkin <vladov3000@gmail.com>
2024-04-12 17:57:01 -0700

    Add release build.

--- build.sh
+++ build.sh
@@ -1,2 +1,11 @@
+set -x
+
+if [ "$1" = "release" ]
+then
+    FLAGS="-O2"
+else
+    FLAGS="-g -fsanitize=undefined"
+fi
+
 mkdir -p build
-clang -g -fsanitize=undefined code/main.c -o build/vzip
+clang $FLAGS code/main.c -o build/vzip
